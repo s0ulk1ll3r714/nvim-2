e0801b21305cc31f23ca636fd0e6972b5e6bf82e
Soul Killer <s0ulk1ll3r714@gmail.com>
2026-02-21 19:08:30 +0530

    init
    made lua-lang-server work proper
    
    toggle lsp on demand
    
    a good config for nvim with lua
    
    added gopls config
    
    chore: remove deno + add tsgo
    
    feat: added more required stuff + tsgo setup
    
    b9c5fdb721e51e2eb06bb9a0f373289445e6a081
    
    chore: back to denols + bun is crap
    
    a5f1974c3a721f451d4e066fe1cd083141cc813d

---
+++ init.lua
@@ -0,0 +1,340 @@
+local need_tailwind = false
+
+---@diagnostic disable-next-line: param-type-mismatch
+vim.api.nvim_create_autocmd("VimResized", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("wincmd =")
+  end,
+})
+
+vim.keymap.set(
+  "t",
+  "<Esc><Esc>",
+  "<C-\\><C-n>",
+  { desc = "Exit terminal mode" }
+)
+
+vim.keymap.set(
+  "n",
+  "<C-h>",
+  "<C-w><C-h>",
+  { desc = "Move focus to the left window" }
+)
+vim.keymap.set(
+  "n",
+  "<C-l>",
+  "<C-w><C-l>",
+  { desc = "Move focus to the right window" }
+)
+vim.keymap.set(
+  "n",
+  "<C-j>",
+  "<C-w><C-j>",
+  { desc = "Move focus to the lower window" }
+)
+vim.keymap.set(
+  "n",
+  "<C-k>",
+  "<C-w><C-k>",
+  { desc = "Move focus to the upper window" }
+)
+
+local toggle_wrap = function()
+  vim.wo.wrap = not vim.wo.wrap
+  if vim.wo.wrap then
+    vim.notify("[toggle_wrap] word wrap enabled", vim.log.levels.WARN)
+  else
+    vim.notify("[toggle_wrap] word wrap disabled", vim.log.levels.WARN)
+  end
+end
+
+vim.keymap.set("n", "<leader>w", toggle_wrap)
+
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NonText guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight NormalFloat guibg=NONE ctermbg=NONE
+  highlight FloatBorder guibg=NONE ctermbg=NONE
+]]
+
+vim.opt.winborder = "single"
+
+vim.opt.listchars = {
+  tab = "▸ ",
+  trail = "·",
+  extends = "›",
+  precedes = "‹",
+  nbsp = "␣",
+}
+
+local indent_group = vim.api.nvim_create_augroup(
+  "FileTypeIndent",
+  { clear = true }
+)
+
+local filetype_settings = {
+  javascript = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  typescript = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  typescriptreact = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  html = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  css = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  scss = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  json = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  yaml = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  vue = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  svelte = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  python = { expandtab = true, shiftwidth = 4, tabstop = 4, softtabstop = 4 },
+  lua = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  go = { expandtab = false, shiftwidth = 4, tabstop = 4, softtabstop = 0 },
+  c = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  cpp = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  rust = { expandtab = true, shiftwidth = 4, tabstop = 4, softtabstop = 4 },
+  java = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  sh = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  bash = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  zsh = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  ruby = { expandtab = true, shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+  php = { expandtab = true, shiftwidth = 4, tabstop = 4, softtabstop = 4 },
+  markdown = { expandtab = true, shiftwidth = 4, tabstop = 4, softtabstop = 4 },
+  make = { expandtab = false, shiftwidth = 4, tabstop = 4, softtabstop = 0 },
+}
+
+for filetype, settings in pairs(filetype_settings) do
+  vim.api.nvim_create_autocmd("FileType", {
+    group = indent_group,
+    pattern = filetype,
+    callback = function()
+      vim.bo.expandtab = settings.expandtab
+      vim.bo.shiftwidth = settings.shiftwidth
+      vim.bo.tabstop = settings.tabstop
+      vim.bo.softtabstop = settings.softtabstop
+    end,
+  })
+end
+
+vim.o.clipboard = "unnamedplus"
+vim.o.signcolumn = "yes"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.clipboard = "unnamedplus"
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.undofile = true
+vim.o.hidden = true
+vim.o.list = true
+vim.o.colorcolumn = "80"
+vim.o.expandtab = true
+vim.o.incsearch = true
+
+vim.keymap.set("i", "jk", "<esc>", { silent = true })
+vim.keymap.set("i", "kj", "<esc>", { silent = true })
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { silent = true })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { silent = true })
+
+vim.pack.add({
+  { src = "https://github.com/vague2k/vague.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/tpope/vim-fugitive" },
+  { src = "https://github.com/echasnovski/mini.nvim" },
+  { src = "https://github.com/cbochs/grapple.nvim" },
+})
+
+require("vague").setup({
+  transparent = true,
+})
+vim.cmd("colorscheme vague")
+
+local oil = require("oil")
+oil.setup({
+  default_file_explorer = true,
+  view_options = {
+    show_hidden = true
+  }
+})
+vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+    end
+  end,
+})
+vim.cmd("set completeopt+=noselect")
+
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { silent = true })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { silent = true })
+vim.keymap.set("n", "gh", vim.lsp.buf.hover, { silent = true })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { silent = true })
+vim.keymap.set("n", "gR", vim.lsp.buf.rename, { silent = true })
+vim.keymap.set("n", "gca", vim.lsp.buf.code_action, { silent = true })
+vim.keymap.set("n", "gcl", vim.lsp.codelens.run, { silent = true })
+vim.keymap.set("n", "gL", vim.lsp.codelens.refresh, { silent = true })
+vim.keymap.set("n", "g=", vim.lsp.buf.format, { silent = true })
+
+local servers = {
+  "lua_ls",
+  "gopls",
+  "denols"
+}
+
+vim.lsp.config["lua_ls"] = {
+  cmd = { '/home/soul/.git-repos/lua-language-server/bin/lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      }
+    }
+  }
+}
+
+vim.lsp.config["gopls"] = {
+  cmd = { 'gopls' },
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  root_markers = { 'go.mod', 'go.work', '.git' },
+}
+
+if need_tailwind then
+  table.insert(servers, "tailwindcss")
+  vim.lsp.config["tailwindcss"] = {
+    cmd = { "tailwindcss-language-server", "--stdio" },
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "html",
+      "css",
+      "svelte",
+      "vue",
+    },
+    root_markers = {
+      "tailwind.config.js",
+      "tailwind.config.ts",
+      "postcss.config.js",
+      "postcss.config.ts",
+      "package.json",
+      ".git",
+    },
+    settings = {
+      tailwindCSS = {
+        includeLanguages = {
+          javascript = "javascript",
+          javascriptreact = "javascriptreact",
+          typescript = "typescript",
+          typescriptreact = "typescriptreact",
+        },
+        experimental = {
+          classRegex = {
+            { "tw`([^`]*)", "'([^']*)'" },
+          }
+        }
+      }
+    }
+  }
+end
+
+vim.lsp.config["denols"] = {
+  cmd = { "deno", "lsp" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  },
+  root_markers = {
+    "deno.json",
+    "deno.jsonc",
+    "deps.ts",
+    "mod.ts",
+    "import_map.json",
+  },
+  init_options = {
+    enable = true,
+    lint = true,
+    unstable = false,
+  },
+  settings = {
+    deno = {
+      enable = true,
+      lint = true,
+      unstable = false,
+      codeLens = {
+        implementations = true,
+        references = true,
+        referencesAllFunctions = true,
+        test = true,
+      },
+      suggest = {
+        imports = {
+          hosts = {
+            ["https://deno.land"] = true,
+          },
+        },
+      },
+    },
+  },
+  single_file_support = true,
+}
+
+---@type boolean
+vim.g.lsp_enabled = false
+
+vim.keymap.set("n", "<leader>l", function()
+  vim.g.lsp_enabled = not vim.g.lsp_enabled
+  if vim.g.lsp_enabled then
+    for _, server in ipairs(servers) do
+      vim.lsp.enable(server, true)
+    end
+    vim.cmd("edit")
+    vim.notify("[LSP] enabled", vim.log.levels.WARN)
+  else
+    for _, server in ipairs(servers) do
+      vim.lsp.enable(server, false)
+    end
+    for _, client in ipairs(vim.lsp.get_clients()) do
+      client:stop()
+    end
+    vim.notify("[LSP] disabled", vim.log.levels.WARN)
+  end
+end)
+
+local mini_pairs = require("mini.pairs")
+mini_pairs.setup()
+
+local mini_pick = require("mini.pick")
+mini_pick.setup()
+
+local mini_extra = require("mini.extra")
+mini_extra.setup()
+
+local mini_statusline = require("mini.statusline")
+mini_statusline.setup()
+
+local mini_comment = require("mini.comment")
+mini_comment.setup()
+
+vim.keymap.set("n", "<leader>sf", "<cmd>Pick files tool='git'<cr>", { silent = true })
+vim.keymap.set("n", "<leader>sg", "<cmd>Pick grep_live<cr>", { silent = true })
+vim.keymap.set("n", "<leader>sb", "<cmd>Pick buffers<cr>", { silent = true })
+vim.keymap.set("n", "<leader>sd", "<cmd>Pick diagnostic<cr>", { silent = true })
+
+local grapple = require("grapple")
+grapple.setup({
+  icons = false
+})
+
+vim.keymap.set("n", "<leader>g", "<cmd>Grapple tag<cr>", { silent = true })
+vim.keymap.set("n", "<leader>G", "<cmd>Grapple toggle_tags<cr>", { silent = true })
+vim.keymap.set("n", "H", "<cmd>Grapple cycle_tags prev<cr>", { silent = true })
+vim.keymap.set("n", "L", "<cmd>Grapple cycle_tags next<cr>", { silent = true })
